3f5464b8caf9d46f301f591463672e9d51fef6e7
Mohammad Abdelaleem <mohammad.abdelaleem@siliconexpert.com>
2026-03-04 13:44:57 +0200

    Initial commit

---
+++ library_website-main/create_db_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Prompt for the version
+read -p "Enter the version tag for this build: " VERSION_TAG
+
+# Define the image name
+IMAGE_NAME="daboortocker/library_website_db:$VERSION_TAG"
+# Check if the Dockerfile exists
+# Build the Docker image
+docker build -t $IMAGE_NAME db_server
+
+# Push the Docker image to Docker Hub
+
+docker push $IMAGE_NAME
+# Check if the push was successful
+echo "Docker image $IMAGE_NAME has been built and pushed to Docker Hub"
